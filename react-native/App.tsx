939c7e9149b3a51ba03068842b0227f01ceb092b
oahehc <oahehc@gmail.com>
2021-03-21 11:20:45 +0900

    chore: react-native for mobile project

---
+++ react-native/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { WebView } from "react-native-webview";
+
+export default function App() {
+  return (
+    <WebView
+      originWhitelist={["*"]}
+      source={{ uri: "http://localhost:3000" }}
+    />
+  );
+}
