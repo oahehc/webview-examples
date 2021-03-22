dede2d12ece5f334a803e7354ae5bfcbcd33be48
oahehc <oahehc@gmail.com>
2021-03-22 21:11:51 +0900

    temp

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,11 +1,48 @@
 import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
-    <WebView
-      originWhitelist={["*"]}
-      source={{ uri: "http://localhost:3000" }}
-    />
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      <TouchableOpacity
+        onPress={() => alert("Hello, world!")}
+        style={styles.button}
+      >
+        <Text style={{ fontSize: 20, color: "#fff" }}>Open</Text>
+      </TouchableOpacity>
+      <MyWebComponent />
+    </View>
   );
 }
+
+class MyWebComponent extends React.Component {
+  render() {
+    return (
+      <View style={{ width: "100%", height: "100%" }}>
+        <WebView
+          originWhitelist={["*"]}
+          source={{ uri: "http://be0e0b752dba.ngrok.io" }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    margin: 10,
+    borderRadius: 5,
+  },
+});
